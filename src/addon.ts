a90c323dda066775083c005da90302114dd0d390
vedantthete <vedantthete@gmail.com>
2025-06-18 22:56:07 +0000

    a

--- src/addon.ts
+++ src/addon.ts
@@ -18,26 +18,19 @@ const manifest: Manifest = {
 };
 
 const builder = new addonBuilder(manifest);
-
+const pxyDomain = 'https://solitary-grass-77bc.hostproxy.workers.dev'
 const parseM3U8 = async function (masterText: any, st: any) {
   const lines = masterText.trim().split('\n');
   const streams: Stream[] = [];
-  streams.push({
-    title: `${st.name ?? "Unknown"}`,
-    description: `Resolution: Auto`,
-    url: `https://solitary-grass-77bc.hostproxy.workers.dev/${st.stream}`,
-    behaviorHints: { notWebReady: true }
-  })
+  
   let baseDomain = st.stream.split('/pl/')[0]
   for (let i = 0; i < lines.length; i++) {
     const line = lines[i];
 
     if (line.startsWith('#EXT-X-STREAM-INF:')) {
       const info = line.replace('#EXT-X-STREAM-INF:', '');
-      const url = `https://solitary-grass-77bc.hostproxy.workers.dev/${baseDomain}${lines[i + 1]?.trim()}`;
-
+      const url = `${pxyDomain}/${baseDomain}${lines[i + 1]?.trim()}`;
       const resolutionMatch = info.match(/RESOLUTION=(\d+x\d+)/);
-
       streams.push({
         title: `${st.name ?? "Unknown"}`,
         description: `Resolution: ${resolutionMatch ? resolutionMatch[1] : null}`,
@@ -47,6 +40,13 @@ const parseM3U8 = async function (masterText: any, st: any) {
 
     }
   }
+  streams.reverse()
+  streams.push({
+    title: `${st.name ?? "Unknown"}`,
+    description: `Resolution: Auto`,
+    url: `${pxyDomain}/${st.stream}`,
+    behaviorHints: { notWebReady: true }
+  })
 
   return streams;
 }
@@ -70,14 +70,7 @@ builder.defineStreamHandler(
         if (st.stream == null) continue;
         let masterRes = await fetch(st.stream)
         let masterText = await masterRes.text()
-        // let newSt = `https://solitary-grass-77bc.hostproxy.workers.dev/${st.stream}`
         streams = await parseM3U8(masterText, st)
-        
-        // streams.push({
-        //   title: st.name ?? "Unknown",
-        //   url: newSt,
-        //   behaviorHints: { notWebReady: true },
-        // });
       }
       return { streams: streams };
     } catch (error) {
